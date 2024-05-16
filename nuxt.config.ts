b12c619ebb7353382f6b0d0cfe342c5e4816f9b7
Balaji <balajiksk153@gmail.com>
2024-05-16 18:55:01 +0530

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   app: {
     baseURL: '/balaji_ksk_git/',
+    buildAssetsDir: 'assets',
     pageTransition: { name: 'page', mode: 'out-in', duration: 250 }
   },
   devtools: { enabled: false },
