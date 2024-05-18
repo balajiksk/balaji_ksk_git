73748d42f1c0c0ecb9576f2c7bc596c42749414a
Balaji <balajiksk153@gmail.com>
2024-05-18 15:39:28 +0530

    commit

--- store/form_pinia.js
+++ store/form_pinia.js
@@ -4,11 +4,11 @@ import { defineStore } from 'pinia';
 export const useCounterStore = defineStore({
   id: 'formPinia',
   state: () => ({
-    name: "",
-    phone: "",
-    email: "",
-    select: "",
-    checkbox: "",
+    name:  JSON.parse(localStorage.getItem('form_name')) || "",
+    phone:  JSON.parse(localStorage.getItem('form_phone')) || "",
+    email:  JSON.parse(localStorage.getItem('form_email')) || "",
+    select:  JSON.parse(localStorage.getItem('form_select')) || "",
+    checkbox:  JSON.parse(localStorage.getItem('form_checkbox')) || ""
   }),
   actions: {
     postvalue(name,phone,email,select,checkbox) {
@@ -17,6 +17,11 @@ export const useCounterStore = defineStore({
       this.email = email;
       this.select = select;
       this.checkbox = checkbox;
+      localStorage.setItem('form_name', JSON.stringify(this.name));
+      localStorage.setItem('form_phone', JSON.stringify(this.phone));
+      localStorage.setItem('form_email', JSON.stringify(this.email));
+      localStorage.setItem('form_select', JSON.stringify(this.select));
+      localStorage.setItem('form_checkbox', JSON.stringify(this.checkbox));
     },
   },
 });
